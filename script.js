30cc1707a69d2b0cdaea8ab3811e8be0c31c1aa2
IshantNikure07 <ishantnikure712@gmail.com>
2024-09-12 18:29:12 +0530

    Tailwind

---
+++ script.js
@@ -0,0 +1,8 @@
+document.querySelectorAll(".hero__rows img").forEach(e => console.log(e))
+
+var a = 
+"https://cdn.prod.website-files.com/659dbdfd5a080be8d3483164/659dbdfd5a080be8d3483181_Ellipse%20845-5.webp"
+
+"https://cdn.prod.website-files.com/659dbdfd5a080be8d3483164/659dbdfd5a080be8d3483180_Ellipse%20845-4.webp"
+
+"https://cdn.prod.website-files.com/659dbdfd5a080be8d3483164/659dbdfd5a080be8d3483182_Ellipse%20845-8.webp"
